eac28bc8230cc05da2b626c4ef8685abbec1b658
zarwaan <zarwaanshroff@gmail.com>
2025-10-13 22:41:26 +0530

    [Practice] More animations for adding and removing nodes

--- client/src/components/Practice.tsx
+++ client/src/components/Practice.tsx
@@ -11,8 +11,8 @@ function Image({index, num} : {num: number, index:number}) {
         useEffect(() => {
             const id = requestAnimationFrame(() => {
                 if(ref.current){
-                    ref.current.style.left= `calc(${shapeConfig[num][index].x} - 3em)`
-                    ref.current.style.top= `calc(${shapeConfig[num][index].y} - 4.5em)`
+                    ref.current.style.left= `calc(${shapeConfig[num][index].x}% - 3em)`
+                    ref.current.style.top= `calc(${shapeConfig[num][index].y}% - 4.5em)`
                 }
             })
             return () => cancelAnimationFrame(id);
@@ -21,11 +21,11 @@ function Image({index, num} : {num: number, index:number}) {
         return (
             <div className='w-[6em] absolute transition-all duration-500' ref={ref}
                 style={{     
-                    left : `calc(${shapeConfig[num-1]?.[index-1]?.x || '50%'} - 3em)`,
-                    top : `calc(${shapeConfig[num-1]?.[index-1]?.y || '50%'} - 4.5em)`
+                    left : `calc(${shapeConfig[num-1]?.[index-1]?.x || 50}% - 3em)`,
+                    top : `calc(${shapeConfig[num-1]?.[index-1]?.y || 50}% - 4.5em)`
                 }}
             >
-                <img src={posters[(index-1) % 3]} alt="Kill Bill" className='rounded-xl'/>
+                <img src={posters[(index-1) % 3]} alt="Kill Bill" className='rounded-xl border-3 border-white'/>
             </div> 
         )
     }
@@ -33,9 +33,12 @@ function Image({index, num} : {num: number, index:number}) {
 
 export default function Practice({n} : {n: number}) {
     return (
-        <div className="m-auto w-8/10 border-1 border-white min-h-full relative">
+        <div className="m-auto w-8/10 border- border-white min-h-full relative grid grid-rows-6 grid-cols-6 *:borde">
             {
                 Array.from({length:n},(_,i)=> <Image key={i} index={i+1} num={n}/>)
+            }
+            {
+                Array.from({length: 36},(_,i) => <div key={i}></div>)
             }
         </div>
     )
