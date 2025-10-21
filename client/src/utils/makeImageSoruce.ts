55e3dfc3c65e4fdae5aaf315e46b9ae5b6b7e250
zarwaan <zarwaanshroff@gmail.com>
2025-10-21 15:43:35 +0530

    Added a no image fallback image

--- client/src/utils/makeImageSoruce.ts
+++ client/src/utils/makeImageSoruce.ts
@@ -1,3 +1,5 @@
+import noImg from '@/assets/images/no_img.jpg'
+
 type sizes = 
     "w92" |
     "w154" |
@@ -8,5 +10,6 @@ type sizes =
     "original"
 
 export default function makeImageSource(imagePath: string, size: sizes ) {
+    if(imagePath === "") return noImg
     return `${import.meta.env.VITE_TMDB_IMAGE_URL_ROOT}/${size}${imagePath}`
 }
